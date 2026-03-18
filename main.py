226eebf1665d257ba5cc2d7e4df8cef74f1337ed
LazyKangeroo <fvantonder00@gmail.com>
2026-03-18 16:32:58 +0200

    Follows
    The snail now follows the cursor and the sprites change depending on the direction it is moving.
     - I want to add an action to happen when the snail is idle but will see what I come up with

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import random
+import math
 import pyautogui
 import sys
 
 from enum import Enum
 
-from PyQt6.QtGui import QPixmap
+from PyQt6.QtGui import QPixmap, QCursor
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel
-from PyQt6.QtCore import Qt, QTimer
+from PyQt6.QtCore import Qt, QTimer, QPoint
 
 # Screen dimensions
 screen_width, screen_height = pyautogui.size()
@@ -24,11 +24,14 @@ class Snail(QWidget):
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
         self.setAttribute(Qt.WidgetAttribute.WA_TransparentForMouseEvents)
 
+        # Point/Pos
+        self.point = QPoint()
+
         # Snail attributes
         self.sprite = 0
         self.count = 0
 
-        # Label (create ONCE)
+        # Label
         self.label = QLabel(self)
 
         # Timer for animation
@@ -43,10 +46,43 @@ class Snail(QWidget):
             self.count = 0
             self.sprite = 1 if self.sprite == 0 else 0
 
-        img = Directions.RIGHT.value[self.sprite]
-        self.updateSnail(img)
+        self.updatePos()
 
-    def updateSnail(self, img):
+        if self.distanceX < 0:
+            direction = Directions.LEFT
+        else:
+            direction = Directions.RIGHT
+
+        img = direction.value[self.sprite]
+        self.updateImg(img)
+
+    def updatePos(self):
+        ## Follow mouse
+        mouse_pos = QCursor.pos()
+
+        #? Get direction/distance of mouse
+        self.geo = self.geometry() # This gets pos and size
+        # print(geo.x(), geo.y(), geo.width(), geo.height())
+
+        #* Calculate distance
+        self.distanceX = mouse_pos.x() - self.geo.x() # if positive then move right, if negative then left
+        self.distanceY = mouse_pos.y() - self.geo.y() # if positve then move down, if negative then move up
+        distance = math.hypot(self.distanceX,self.distanceY)
+
+        if distance < 5:
+            return
+
+        speed = 8
+        #  direction vector
+        velocoityX = (self.distanceX / distance) * speed
+        velocoityY = (self.distanceY / distance) * speed
+
+        new_x = self.geo.x() + velocoityX
+        new_y = self.geo.y() + velocoityY
+
+        self.move(int(new_x), int(new_y))
+
+    def updateImg(self, img):
         pixmap = QPixmap(img)
 
         self.label.setPixmap(pixmap)
